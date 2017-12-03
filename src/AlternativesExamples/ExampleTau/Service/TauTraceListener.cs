43ea3c33c993ca47ff857ac61a07f776fdf492d4
Baurzhan Atanov <atanov.b@gmail.com>
2017-12-03 22:31:30 +0600

    Added custom trace listener

---
+++ src/AlternativesExamples/ExampleTau/Service/TauTraceListener.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Service
+{
+    public class TauTraceListener : TraceListener
+    {
+        private const string LogFile = @"C:\SoapLog\Tau\Service\TauTraceListener.xml";
+
+        public override void Write(string message)
+        {
+            File.AppendAllText(LogFile, message + Environment.NewLine + Environment.NewLine);
+        }
+
+        public override void WriteLine(string message)
+        {
+            File.AppendAllText(LogFile, message + Environment.NewLine + Environment.NewLine);
+        }
+    }
+}
