789867c2e2d6b1bbe5a5d18cc9d3a6e84eeabcf7
Baurzhan Atanov <atanov.b@gmail.com>
2017-12-10 12:38:03 +0600

    Added free check of source code by PVS-Studio

--- src/Library/WcfSoapLogger/EncodingExtension/LoggingEncoderFactory.cs
+++ src/Library/WcfSoapLogger/EncodingExtension/LoggingEncoderFactory.cs
@@ -1,4 +1,6 @@
-﻿using System.ServiceModel.Channels;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+using System.ServiceModel.Channels;
 
 namespace WcfSoapLogger.EncodingExtension
 {
--- src/Library/WcfSoapLogger/Exceptions/ErrorBody.cs
+++ src/Library/WcfSoapLogger/Exceptions/ErrorBody.cs
@@ -1,4 +1,6 @@
-﻿using System.Security;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+using System.Security;
 using System.Text;
 
 namespace WcfSoapLogger.Exceptions
